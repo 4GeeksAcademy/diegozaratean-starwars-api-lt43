9a5d0a07093615059e0fcecb552f300f17907da9
diego <diegozaratean@gmail.com>
2025-02-24 22:48:26 +0000

    begin staerwars api implementation

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Empresa
 #from models import Person
 
 app = Flask(__name__)
@@ -31,6 +31,8 @@ setup_admin(app)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
+# INICIO DEL CODIGO
+
 # generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
@@ -39,11 +41,51 @@ def sitemap():
 @app.route('/user', methods=['GET'])
 def handle_hello():
 
+    print('get all users')
+    user = User.query.get(1)
+    print(user)
+    print(user.serialize())
+
+    all_users = User.query.all()
+    print(all_users)
+    results = list(map(lambda user: user.serialize(),all_users))
+    print(results)
+    
+
+
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "lista de todos los usuairos ",
+        "users": results
     }
 
     return jsonify(response_body), 200
+
+@app.route('/test', methods=['GET'])
+def test():
+
+    response_body = {
+        "msg": "esto esw una prueba  "
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/empresa', methods=['GET'])
+def get_empresas():
+    all_companies = Empresa.query.all()
+    results = list(map(lambda company: company.serialize(),all_companies))
+
+    return jsonify(results), 200
+
+
+@app.route('/empresa/<int:company_id>', methods=['GET'])
+def get_compnay(company_id):
+    company = Empresa.query.get(company_id)
+    return jsonify(company.serialize()), 200
+
+
+
+# FIN DEL CODIGO
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -9,11 +9,30 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<Usuario %r>' % self.email
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+    
+
+class Empresa(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(120), unique=True, nullable=False)
+    ciudad = db.Column(db.String(80), unique=False, nullable=False)
+    slogan = db.Column(db.String(120), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Empresa %r>' % self.nombre
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.nombre,
+            "city": self.ciudad,
+            "slogan": self.slogan,
             # do not serialize the password, its a security breach
         }
